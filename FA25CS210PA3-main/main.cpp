6dd6f0198c5767747de78fabf96d7e75c1bf2914
Dante-Manansala <161496718+Dante-Manansala@users.noreply.github.com>
2025-12-06 17:32:41 -0800

    DFS almost done (has errors)
    Running into maybe runtime error and output errors. commiting to have help looking as to why

--- FA25CS210PA3-main/main.cpp
+++ FA25CS210PA3-main/main.cpp
@@ -120,11 +120,46 @@ void printPath(pair<int,int> exitcell,
 // ----------------------------------------------------------
 /// NOTE: make Depth-First Search(DFS)
 /// has 8 inputs based off main : dfs(ent_r, ent_c, maze, visited, parent_r, parent_c, exit_r, exit_c)
-bool dfs() {
-    // Your code here
+bool dfs(int ent_r, int ent_c,
+         const vector<vector<int>>& maze,
+         vector<vector<bool>>& visited,
+         vector<vector<int>>& parent_r, vector<vector<int>>& parent_c,
+         int exit_r, int exit_c) {
 
+    ///NOTE: current is current cell
+    // check if current cell is viable
+    // if not in bounds, false (helps avoid bound errors)
+    if (ent_r < 0 || ent_r >= maze.size() || ent_c < 0 || ent_c >= maze[0].size()) return false;
+    // if current == wall, false
+    if (maze[ent_r][ent_c] == 1) return false;
+    // if visited == true, false
+    if (visited[ent_r][ent_c]) return false;
 
-    return false; // default for now
+    //mark current as visited
+    visited[ent_r][ent_c] = true;
+
+    ///EXIT CONDITION: recursion
+    // if current == exit, true
+    if (ent_r == exit_r && ent_c == exit_c) return true;
+
+    // prep for each adjacent cell for recursion
+    for (int x = 0; x < 4; x++) {
+
+        // created for easier reading
+        int ar = ent_r + dr[x]; //adjacent row
+        int ac = ent_c + dc[x]; //adjacent column
+        
+        // mark parents for adjacent/new cell in recursion
+        parent_r[ar][ac] = ent_r;
+        parent_c[ar][ac] = ent_c;
+
+        /// RECURSION
+        // Recursion adjacent cell of current
+        if (dfs(ar, ac, maze, visited, parent_r, parent_c, exit_r, exit_c)) return true;
+    }
+
+    ///EXIT CONDITION: recursion
+    return false; // if exit not reachable, false
 }
 
 
@@ -132,12 +167,12 @@ bool dfs() {
 // MAIN PROGRAM (students add DFS calls and logic)
 // ----------------------------------------------------------
 int main() {
-    int N, M; // height/width?
+    int N, M; // N = columns, M = rows
 
     cout << "Enter maze dimensions N M: ";
     cin >> N >> M;
 
-    vector<vector<int>> maze(N, vector<int>(M));
+    vector<vector<int>> maze(N, vector<int>(M)); // vector within vector
     generateMaze(maze, N, M);
 
     // Pick entrance and exit
@@ -165,19 +200,14 @@ int main() {
     // STUDENT WORK:
     // TODO: Call your DFS, track visited, and fill parent_r and parent_c
     // ------------------------------------------------------
-    // bool found = dfs(ent_r, ent_c, maze, visited, parent_r, parent_c, exit_r, exit_c);
-
-
+     bool found = dfs(ent_r, ent_c, maze, visited, parent_r, parent_c, exit_r, exit_c);
 
     // ------------------------------------------------------
     // STUDENT WORK:
     // TODO: If found, print the path
     // ------------------------------------------------------
-    // if (found) {
-    //     printPath(exitcell, parent_r, parent_c, ent_r, ent_c);
-    // } else {
-    //     cout << "\nNo path exists.\n";
-    // }
+     if (found) {printPath(exitcell, parent_r, parent_c, ent_r, ent_c);
+     } else {cout << "\nNo path exists.\n";}
 
     return 0;
 }
